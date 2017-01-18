afdd3b4f20e725bf992af4f6bc0751348c264e6f
ravi128sahu <ravi128sahu@gmail.com>
2017-01-18 12:54:34 +0530

    Removing unwanted files.

--- src/main/java/com/ravi/user/dao/UserRegisterDao.java
+++
@@ -1,8 +0,0 @@
-package com.ravi.user.dao;
-
-import com.ravi.user.model.UserRegisterModel;
-
-public interface UserRegisterDao {
-	public void addUser(UserRegisterModel userModel);
-	public UserRegisterModel getLoginCredentials(String userName);
-}
