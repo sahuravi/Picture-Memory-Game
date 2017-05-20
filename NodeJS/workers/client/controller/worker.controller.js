4a76431bef630381061996c0b5154df5f30a3421
Ravi\Sahu <ravi128sahu@gmail.com>
2017-05-21 04:22:56 +0530

    updating code.

--- NodeJS/workers/client/controller/worker.controller.js
+++ NodeJS/workers/client/controller/worker.controller.js
@@ -1,79 +1,19 @@
 let myApp = angular.module('workerApp', []);
 
-myApp.run(['$rootScope', '$http', function ($rootScope, $http) {
+myApp.controller('WorkerController', ['$rootScope', '$scope', '$timeout', 'workerService', function ($rootScope, $scope, $timeout, workerService) {
 
-    let workerJson = {
-        "workers": [
-            {
-                "name": "Tim Dowd",
-                "mobileNumber": "+122-7485964152",
-                "taskAssigned": "Manage files",
-                "taskDuration": 5
-            },
-            {
-                "name": "Jane Mistro",
-                "mobileNumber": "+122-1527485964",
-                "taskAssigned": "Verify machine status",
-                "taskDuration": 10
-            },
-            {
-                "name": "Jerry Lucas",
-                "mobileNumber": "+122-9641527485",
-                "taskAssigned": "Monitor power management",
-                "taskDuration": 3
-            },
-            {
-                "name": "Tyra Kindo",
-                "mobileNumber": "+122-5964157482",
-                "taskAssigned": ""
-            },
-            {
-                "name": "Cesar Monk",
-                "mobileNumber": "+122-4152748596",
-                "taskAssigned": "Order Management",
-                "taskDuration": 5
-            },
-            {
-                "name": "Zed Mills",
-                "mobileNumber": "+122-6415274859",
-                "taskAssigned": ""
-            },
-            {
-                "name": "Kera Hoadely",
-                "mobileNumber": "+122-7596415482",
-                "taskAssigned": ""
-            },
-            {
-                "name": "Craig Hoadely",
-                "mobileNumber": "+122-5967484152",
-                "taskAssigned": "Insert Cotton",
-                "taskDuration": 7
-            },
-            {
-                "name": "Dee Sather",
-                "mobileNumber": "+122-9647485152",
-                "taskAssigned": ""
-            }
-        ]
-    };
-    $rootScope.workersData = workerJson.workers;
-
-    // $http.get("./workers.json")
-    //     .then(function (response) {
-    //         debugger
-    //         $rootScope.workerData = response.workers;
-    //     });
-}])
-
-myApp.controller('WorkerController', ['$rootScope', '$scope', '$element', '$timeout', function ($rootScope, $scope, $element, $timeout) {
-
-    //console.log($rootScope.workersData);
     $scope.selectedWorker = null;
     $scope.workAssigned = false;
     $scope.taskName = null;
     $scope.taskDuration = null;
-
-    let workersArray = $rootScope.workersData;
+    
+    workerService.getWorkersData()
+        .then((workersData) => {
+            $scope.workersData = workersData;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 
     $scope.assignWorker = function (worker) {
         if (worker.taskAssigned === "") {
@@ -87,9 +27,7 @@ myApp.controller('WorkerController', ['$rootScope', '$scope', '$element', '$time
     }
 
     $scope.assignTask = function () {
-        
-        $rootScope.workersData.forEach(function (worker) {
-            
+        $scope.workersData.forEach(function (worker) {
             if (worker.mobileNumber === $scope.selectedWorker.mobileNumber) {
                 worker.taskAssigned = $scope.taskName;
                 worker.taskDuration = $scope.taskDuration;
