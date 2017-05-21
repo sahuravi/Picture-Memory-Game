4b58e67788b163df4e8f4d9d0d100cc51799cb15
Ravi\Sahu <ravi128sahu@gmail.com>
2017-05-21 12:33:07 +0530

    updating code.

--- NodeJS/workers/client/directive/worker-directive.js
+++ NodeJS/workers/client/directive/worker-directive.js
@@ -4,21 +4,8 @@
     angular.module('workerApp')
         .directive('workerDirective', ['$timeout', function ($timeout) {
             return {
-                restrict: 'A',
-                scope: {
-                    'updated': '=owlDataUpdated',
-                    'owlOptions': '=owlOptions'
-                },
-                link: function ($scope, $element, attrs, controller) {
-                    $timeout(function () {
-                        var listener = $scope.$watch('updated', function () {
-                            if ($scope.updated != undefined) {
-                                $element.owlCarousel($scope.owlOptions);
-                                listener();
-                            }
-                        });
-                    });
-                }
-            };
+                restrict: 'E',
+                templateUrl: '../view/worker.template.html'
+            }
         }]);
 }());
