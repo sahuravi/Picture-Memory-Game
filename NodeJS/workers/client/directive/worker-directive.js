0683de5f1fb66007a8e3ca4fe31bafb85b7f1497
ravi128sahu <ravi128sahu@gmail.com>
2017-05-23 13:50:37 +0530

    adding code in directive.

--- NodeJS/workers/client/directive/worker-directive.js
+++ NodeJS/workers/client/directive/worker-directive.js
@@ -5,7 +5,14 @@
         .directive('workerDirective', ['$timeout', function ($timeout) {
             return {
                 restrict: 'E',
-                templateUrl: '../view/worker.template.html'
+                templateUrl: '../view/worker.template.html',
+                scope: {
+                    workerDetail: '=',
+                    availability: '&'
+                },
+                controller: function($scope, $element, $attrs) {
+                    
+                }
             }
         }]);
 }());
