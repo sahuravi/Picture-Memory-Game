62a283daec252ad2e6c3f36ea290d1294582afdb
ravi128sahu <ravi128sahu@gmail.com>
2017-06-05 15:14:04 +0530

    adding currying example

---
+++ JavaScript/Currying/currying.example.js
@@ -0,0 +1,17 @@
+function sum() {
+    var args = Array.prototype.slice.call(arguments);
+
+    var func = function () {
+        var nxtArgs = Array.prototype.slice.call(arguments);
+        args = args.concat(nxtArgs);
+        return func;
+    };
+
+    func.toString = func.valueOf = function () {
+        return args.reduce(function (acc, nxtValue) {
+            return acc + nxtValue;
+        })
+    };
+
+    return func;
+}
