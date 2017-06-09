5f25e5a838ee7f49641757c0e51db8895f4ede3a
ravi128sahu <ravi128sahu@gmail.com>
2017-06-09 13:03:54 +0530

    updating code to dynamic change file on server and send it back to client.

--- NodeJS/chat/chat-server.js
+++ NodeJS/chat/chat-server.js
@@ -7,16 +7,36 @@ server.listen(9000, () => {
     console.log("chat server listening on port 9000")
 });
 
-io.on('connection', function (socket) {debugger;
+io.on('connection', function (socket) {
+    debugger;
+    let olderData = null;
     setInterval(() => {
-        fs.readFile(__dirname + '/index.html', (err, data) => {
+        fs.readFile(__dirname + '/test.txt', (err, data) => {
             if (err) {
+                socket.emit('news', {
+                    data: Error.message
+                });
+            } else {
+                if (olderData !== data.toString()) {
+                    console.log("aaya..");
+                    olderData = data.toString();
+                    socket.emit('news', {
+                        data: olderData
+                    });
+                }
             }
-            socket.emit('news', {data: data.toString()});
         });
-    }, 10000);
-    
-    socket.on('my other event', function (data) {
-        console.log(data);
+    }, 1000);
+});
+
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+
+        res.writeHead(200);
+        res.end(data);
     });
 });
