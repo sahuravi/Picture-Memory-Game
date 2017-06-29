3418927d7640ba3fe9910f58c3e92912ab738852
ravi128sahu <ravi128sahu@gmail.com>
2017-06-29 18:37:58 +0530

    adding map pollyfill function.

---
+++ JavaScript/practice/map-pollyfill.js
@@ -0,0 +1,14 @@
+Array.prototype.myMap = function (callback, thisArgs) {
+    var arr = [];
+    for(var i in this) {
+        if(this.hasOwnProperty(i)) {
+            arr[i] = callback.call(thisArgs, this[i], i, this);    
+        }
+    }
+    return arr;
+}
+
+var arr = [1,2,3];
+arr.myMap(function(a){
+    console.log(a);
+});
