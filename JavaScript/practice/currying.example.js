e17ad3c0de0c280618fb6384f593de5bd421a8ea
ravi128sahu <ravi128sahu@gmail.com>
2017-07-12 19:09:28 +0530

    adding pollyfill for Object.create()

--- JavaScript/practice/currying.example.js
+++ JavaScript/practice/currying.example.js
@@ -79,3 +79,68 @@ function demethodize(fn) {
 }
 demethodize(Number.prototype.add)(5, 7);
 
+/* Write a function composeu that takes two unary funcitons and returns a unary function that calls them both.
+ *   Ex:  composeu(double, square)(4);      // 64
+ */
+
+function composeu(fn1, fn2) {
+    return function (arg) {
+        return fn2(fn1(arg));
+    }
+}
+
+composeu(double, square)(4); // before calling this line we need to define both function i.e double and square.
+
+
+/* Write a function composeb that takes two binary funcitons and returns a function that calls them both.
+ *   Ex:  composeu(addf, mult)(4, 5, 2);      // 18
+ */
+
+function composeu(fn1, fn2) {
+    return function (a, b, c) {
+        return fn2(fn1(a, b), c);
+    }
+}
+
+composeu(double, square)(4);
+
+
+/* Write a function that allows another function to only be called once.
+ *   Ex:  add_once = once(add);      // 18
+ *      add_once(2,3);      // 5
+ *      add_once(4,7);      // throw error
+ */
+
+function once(fn) {
+    return function () {
+        let F = fn;
+        fn = null;
+        return F === null ? new Error("You cann't call this funciton twice") : F.apply(this, arguments);
+    }
+}
+
+var add_once = once(addf);
+add_once(8, 9);
+add_once(8, 9);
+
+
+/* Write a factory function that returns two functions that implement an Up/Down counter.
+ *   Ex:  counter = counterf(10);
+ *      counter.inc();      // 11
+ *      counter.dec();      // 10
+ */
+
+function counterf(number) {
+    return {
+        inc: function () {
+            return ++number;
+        },
+        dec: function () {
+            return --number;
+        }
+    }
+}
+
+var counter = counterf(5);
+counter.inc(); // 6
+counter.dec(); // 5
